4b6698311b0e50055d432231b728ea7f589f34e2
emmabell42 <emmabell42@gmail.com>
2017-05-01 18:19:36 +0100

    Update GeneSetEnrichmentAnalysis.R

--- GeneSetEnrichmentAnalysis.R
+++ GeneSetEnrichmentAnalysis.R
@@ -3,7 +3,7 @@
 ## This script performs Gene Set Enrichment Analysis of candidate genes in 
 ## ConsensusPathDB pathways. It requires a list of genes with an associated
 ## statistic and the ConsensusPathDB list of biological pathways with their
-## genes identified by gene symbol
+## genes identified by gene symbol. Input variables are capitalised.
 ##
 ################################################################################
 #
@@ -31,8 +31,6 @@ pathway.enrichments <- array(NA,dim=c(nrow(pathways),1))
 rownames(pathway.enrichments) <- pathways$pathway
 library(limma)
 for(j in 1:nrow(pathways)){
-		# default is for t-statistics. if using f-statistics (as in Fisher's meta-analysis statistics), comment out line 20 and uncomment line 21
-                pathway.enrichments[j,1] <- geneSetTest(index=which(GENES[,3] %in% pathway.genes[[j]]),statistics=STATISTICS,ranks.only=FALSE,type="t",alternative="up")
-		#pathway.enrichments[j,i] <- geneSetTest(selected=which(rownames(statistics.table) %in% pathway.genes[[j]]),statistics=statistics.table[,i],type="f",ranks.only=FALSE)
+		pathway.enrichments[j,1] <- geneSetTest(index=which(GENES[,3] %in% pathway.genes[[j]]),statistics=STATISTICS,ranks.only=FALSE,type="t",alternative="up")
 }
 pathway.enrichments[,1] <- p.adjust(pathway.enrichments[,1],method="BH")
